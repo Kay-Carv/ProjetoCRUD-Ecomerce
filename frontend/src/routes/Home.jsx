5085d686a80b164240b6dacc6164db6c4f0c6347
Kayque <kayquecarvalho020@gmail.com>
2025-05-23 23:43:12 -0300

    Feat: Preparando Home para o recebimento dos produtos

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import { useState, useEffect } from "react"
+import Produto from "./Produto"
 
 const Home = () => {
+    const [produto, setProduto]= useState([]);
+    const [loading, setLoading]= useState(true);
+    const [error, setError]= useState(null);
+
+useEffect( () =>{
+  fetch('http://localhost:3001/produtos')
+  .then(response =>{
+    if(!response.ok){
+      console.log("Erro ao trazer os produtos")
+    }
+    return response.json();
+  })
+  .then(data=>{
+    setProduto(data);
+    setLoading(false)
+  })
+  .catch(error => {
+    setError(error);
+    setLoading(false);
+    console.log("Erro ao buscar os produtos")
+  }) 
+}, [])
+
+if(loading){
+  return <div>Carregando produtos...</div>
+}
+if(error){
+  return <div>Erro ao carregar os produtos: {error.menssage}</div>
+}
+
   return (
-    <div>
-      
+
+    // flex-wrap para quebrar em colunas
+    <div className="flex justify-center flex-wrap">
+      {produto.map((produto)=>{
+        <Produto key={produto.id} produto={produto}/>
+      })}
     </div>
   )
 }
