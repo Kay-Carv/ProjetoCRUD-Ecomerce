cfff599f92b14e25678e5ce0a1b7f3d0052168a0
Kayque <kayquecarvalho020@gmail.com>
2025-05-24 23:12:25 -0300

    Feat: Criando rotas e importando carrinho no App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,23 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Header from './components/Header'
+import Home from './routes/Home'
+import Carrinho from './routes/Carrinho'
+
 function App() {
 
   return (
-    <>
-      
-    </>
+    <Router>
+      <div className="min-h-screen flex flex-col">
+        <main className="grow">
+        <Header>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/carrinho" element={<Carrinho/>}/>
+          </Routes>
+        </Header>
+        </main>
+      </div>
+    </Router>
   )
 }
 
