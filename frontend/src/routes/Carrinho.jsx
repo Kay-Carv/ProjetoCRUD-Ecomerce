6b6ffc7578b728b6df7dc1bb58ab10b779e91cff
Kayque <kayquecarvalho020@gmail.com>
2025-05-25 22:17:16 -0300

    Feat: Criando a visualização e botões para as funcionalidades dos itens no carrinho

--- frontend/src/routes/Carrinho.jsx
+++ frontend/src/routes/Carrinho.jsx
@@ -41,6 +41,41 @@ const Carrinho = () => {
 
   return (
     <div className='p-4'>
+      <h2 className='text-2xl font-semibold mb-4'> Seu carrinho </h2>
+      <ul className='space-y-2'>
+        {carrinho.map((item) =>(
+          <li className='flex justify-between items-center border-b pb-2' key={item.id}>
+            {/* Talvez deixar uma classname individual para cada um dos Span*/}
+            <span className='text-lg'>
+              {item.nome} - ${item.preco} x {item.quantidade}
+            </span>
+            <button
+              className='bg-red-500 hover:bg-red-700'
+              onClick={() => handleRemoverItem(item)}
+            >
+              Removem
+            </button>
+          </li>
+        ))}
+      </ul>
+      <h3 className='text-xl font-semibold mt-4'>Total: ${total.toFixed(2)}</h3>
+
+      <div className='flex justify-start mt-4 space-x-2'>
+        <button
+          className='bg-red-500 hover:bg-red-700 cursor-pointer text-white font-semibold py-2 px-4 rounded-md'
+          onClick={hanldeLimparCarrinho}
+        >
+          Limpar Carrinho
+        </button>
+
+        <button
+          className='bg-blue-500 hover:bg-blue-700 text-white cursor-pointer font-semibold py-2 px-4 rounded-md'
+          onClick={handleContinuarCompra}
+        >
+          Continuar Comprando
+        </button>
+
+      </div>
       
     </div>
   )
