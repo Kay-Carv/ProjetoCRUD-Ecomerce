c3bc41785d7124092d85bd70458d311513bc882a
Kayque <kayquecarvalho020@gmail.com>
2025-05-28 23:46:59 -0300

    Fix: Arrumando a requisição dos produtos que não estavam aparecendo na Home

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -6,39 +6,40 @@ const Home = () => {
     const [loading, setLoading]= useState(true);
     const [error, setError]= useState(null);
 
-useEffect( () =>{
-  fetch('http://localhost:3001/produtos')
-  .then(response =>{
-    if(!response.ok){
-      console.log("Erro ao trazer os produtos")
-    }
-    return response.json();
-  })
-  .then(data=>{
-    setProduto(data);
-    setLoading(false)
-  })
-  .catch(error => {
-    setError(error);
-    setLoading(false);
-    console.log("Erro ao buscar os produtos")
-  }) 
-}, [])
+    // useeffect taz a requisição dos produtos do server para aparecer na página
+  useEffect( () =>{
+    fetch('http://localhost:3001/produtos')
+    .then(response =>{
+      if(!response.ok){
+        console.log("Erro ao trazer os produtos")
+      }
+      return response.json();
+    })
+    .then(data=>{
+      setProduto(data);
+      setLoading(false)
+    })
+    .catch(error => {
+      setError(error);
+      setLoading(false);
+      console.log("Erro ao buscar os produtos")
+    }) 
+  }, [])
 
-if(loading){
-  return <div>Carregando produtos...</div>
-}
-if(error){
-  return <div>Erro ao carregar os produtos: {error.menssage}</div>
-}
+  if(loading){
+    return <div>Carregando produtos...</div>
+  }
+  if(error){
+    return <div>Erro ao carregar os produtos: {error.menssage}</div>
+  }
 
   return (
 
     // flex-wrap para quebrar em colunas
     <div className="flex justify-center flex-wrap">
-      {produto.map((produto)=>{
-        <Produto key={produto.id} produto={produto}/>
-      })}
+      {produto.map((produto)=>(
+      <Produto key={produto.id} produto={produto}/>
+      ))}
     </div>
   )
 }
